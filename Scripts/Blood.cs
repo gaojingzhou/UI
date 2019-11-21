eea374ee9b5831a5cc40da759360580c5828fb85
gaojingzhou <54970231+gaojingzhou@users.noreply.github.com>
2019-11-21 15:01:11 +0800

    Add files via upload

---
+++ Scripts/Blood.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Blood : MonoBehaviour
+{
+    public float blood;
+    public float res;
+    public Rect rect;
+    public Rect add;
+    public Rect minus;
+    public Rect layout;
+    // Start is called before the first frame update
+    void Start()
+    {
+        res = blood;
+        rect = new Rect(200, 100, 200, 15);
+        add = new Rect(420, 100, 20, 20);
+        minus = new Rect(160, 100, 20, 20);
+        layout = new Rect(150, 70, 300, 50);
+    }
+
+    private void OnGUI()
+    {
+        
+        if (GUI.Button(add, "+"))
+        {
+            res = res + 0.1f > 1 ? 1 : res + 0.1f;
+        }
+        if (GUI.Button(minus, "-"))
+        {
+            res = res - 0.1f < 0 ? 0 : res - 0.1f;
+        }
+        GUI.Box(layout, "Blood");
+        blood = Mathf.Lerp(blood, res, 0.05f);
+        GUI.HorizontalScrollbar(rect, 0, blood, 0, 1);
+    }
+}
---
+++ Scripts/Blood2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Blood2 : MonoBehaviour
+{
+    public float blood;
+    public float res;
+    public Rect add;
+    public Rect minus;
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        res = blood;
+        add = new Rect(420, 100, 20, 20);
+        minus = new Rect(160, 100, 20, 20);
+    }
+
+    private void OnGUI()
+    {
+
+        if (GUI.Button(add, "+"))
+        {
+            res = res + 0.1f > 1 ? 1 : res + 0.1f;
+        }
+        if (GUI.Button(minus, "-"))
+        {
+            res = res - 0.1f < 0 ? 0 : res - 0.1f;
+        }
+        blood = Mathf.Lerp(blood, res, 0.05f);
+        slider.value = blood;
+    }
+}
